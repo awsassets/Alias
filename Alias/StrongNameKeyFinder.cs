9b6679314be9601f8c41486f46fc283109f99d0c
Simon Cropp <simon.cropp@gmail.com>
2021-11-01 20:13:38 +1100

    Update StrongNameKeyFinder.cs

--- Alias/StrongNameKeyFinder.cs
+++ Alias/StrongNameKeyFinder.cs
@@ -1,6 +1,5 @@
 using System;
 using System.IO;
-using System.Linq;
 
 #if (NETSTANDARD)
 using StrongNameKeyPair = Mono.Cecil.StrongNameKeyPair;
@@ -20,68 +19,39 @@ public partial class Processor
             return;
         }
 
-        var keyFilePath = GetKeyFilePath();
-        if (keyFilePath != null)
+        if (keyFile == null)
         {
-            if (!File.Exists(keyFilePath))
+            return;
+        }
+
+        var fileBytes = File.ReadAllBytes(keyFile);
+
+        if (!delaySign)
+        {
+            try
             {
-                throw new WeavingException($"KeyFilePath was defined but file does not exist. '{keyFilePath}'.");
+                logger.LogDebug("Extract public key from key file for signing.");
+
+                StrongNameKeyPair = new(fileBytes);
+                // Ensure that we can generate the public key from the key file. This requires the private key to
+                // work. If we cannot generate the public key, an ArgumentException will be thrown. In this case,
+                // the assembly is delay-signed with a public only key-file.
+                // Note: The NETSTANDARD implementation of StrongNameKeyPair.PublicKey does never throw here.
+                PublicKey = StrongNameKeyPair.PublicKey;
+                return;
             }
-
-            var fileBytes = File.ReadAllBytes(keyFilePath);
-
-            if (!delaySign)
+            catch (ArgumentException)
             {
-                try
-                {
-                    logger.LogDebug("Extract public key from key file for signing.");
-
-                    StrongNameKeyPair = new(fileBytes);
-                    // Ensure that we can generate the public key from the key file. This requires the private key to
-                    // work. If we cannot generate the public key, an ArgumentException will be thrown. In this case,
-                    // the assembly is delay-signed with a public only key-file.
-                    // Note: The NETSTANDARD implementation of StrongNameKeyPair.PublicKey does never throw here.
-                    PublicKey = StrongNameKeyPair.PublicKey;
-                    return;
-                }
-                catch (ArgumentException)
-                {
-                    logger.LogWarning("Failed to extract public key from key file, fall back to delay-signing.");
-                }
+                logger.LogWarning("Failed to extract public key from key file, fall back to delay-signing.");
             }
-
-            // Fall back to delay signing, this was the original behavior, however that does not work in NETSTANDARD (s.a.)
-            logger.LogDebug("Prepare public key for delay-signing.");
-
-            // We know that we cannot sign the assembly with this key-file. Let's assume that it is a public
-            // only key-file and pass along all the bytes.
-            StrongNameKeyPair = null;
-            PublicKey = fileBytes;
-        }
-    }
-
-    string? GetKeyFilePath()
-    {
-        if (keyFile != null)
-        {
-            keyFile = Path.GetFullPath(keyFile);
-            logger.LogDebug($"Using strong name key from KeyFilePath '{keyFile}'.");
-            return keyFile;
         }
 
-        var assemblyKeyFileAttribute = ModuleDefinition
-            .Assembly
-            .CustomAttributes
-            .FirstOrDefault(x => x.AttributeType.Name == "AssemblyKeyFileAttribute");
-        if (assemblyKeyFileAttribute != null)
-        {
-            var keyFileSuffix = (string)assemblyKeyFileAttribute.ConstructorArguments.First().Value;
-            var keyFilePath = Path.Combine(intermediateDirectory, keyFileSuffix);
-            logger.LogDebug($"Using strong name key from [AssemblyKeyFileAttribute(\"{keyFileSuffix}\")] '{keyFilePath}'");
-            return keyFilePath;
-        }
+        // Fall back to delay signing, this was the original behavior, however that does not work in NETSTANDARD (s.a.)
+        logger.LogDebug("Prepare public key for delay-signing.");
 
-        logger.LogDebug("No strong name key found");
-        return null;
+        // We know that we cannot sign the assembly with this key-file. Let's assume that it is a public
+        // only key-file and pass along all the bytes.
+        StrongNameKeyPair = null;
+        PublicKey = fileBytes;
     }
 }
